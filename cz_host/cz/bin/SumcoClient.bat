81eb49701bf8a7ebb87b3e3b9bb84141a4b97042
人見　貴徳 <miniyon1103@gmail.com>
2014-10-05 11:46:16 +0900

    cz_host

--- cz_host/cz/bin/SumcoClient.bat
+++
@@ -1,7 +0,0 @@
-set PATH=%PATH%;C:\jdk1.5.0_08\bin;c:\horb2.0\bin
-set CLASSPATH=%CLASSPATH%;.;c:\horb2.0\lib\horb20.jar;c:\horb2.0\classes
-set CLASSPATH=%CLASSPATH%;c:\cz\Lib\classes12.zip;c:\cz\Lib\CZClass.jar;c:\cz\Lib\CZSystemLib.jar
-
-cd c:\cz\
-C:\jdk1.5.0_08\bin\java -Xms128M -Xmx256M cz.CZMain
-exit
